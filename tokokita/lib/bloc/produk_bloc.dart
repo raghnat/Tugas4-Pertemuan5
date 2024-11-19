ad05b2c35c00063ad17ef46aa543477e789a8e71
raghnat <rayhan.aghnat@mhs.unsoed.ac.id>
2024-11-19 09:54:54 +0700

    push

---
+++ tokokita/lib/bloc/produk_bloc.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:tokokita/helpers/api.dart';
+import 'package:tokokita/helpers/api_url.dart';
+import 'package:tokokita/model/produk.dart';
+
+class ProdukBloc {
+  static Future<List<Produk>> getProduks() async {
+    String apiUrl = ApiUrl.listProduk;
+    var response = await Api().get(apiUrl);
+    var jsonObj = json.decode(response.body);
+    List<dynamic> listProduk = (jsonObj as Map<String, dynamic>)['data'];
+    List<Produk> produks = [];
+    for (int i = 0; i < listProduk.length; i++) {
+      produks.add(Produk.fromJson(listProduk[i]));
+    }
+    return produks;
+  }
+
+  static Future addProduk({Produk? produk}) async {
+    String apiUrl = ApiUrl.createProduk;
+
+    var body = {
+      "kode_produk": produk!.kodeProduk,
+      "nama_produk": produk.namaProduk,
+      "harga": produk.hargaProduk.toString()
+    };
+
+    var response = await Api().post(apiUrl, body);
+    var jsonObj = json.decode(response.body);
+    return jsonObj['status'];
+  }
+
+  static Future updateProduk({required Produk produk}) async {
+    String apiUrl = ApiUrl.updateProduk(int.parse(produk.id!));
+    print(apiUrl);
+
+    var body = {
+      "kode_produk": produk.kodeProduk,
+      "nama_produk": produk.namaProduk,
+      "harga": produk.hargaProduk.toString()
+    };
+    print("Body : $body");
+    var response = await Api().put(apiUrl, jsonEncode(body));
+    var jsonObj = json.decode(response.body);
+    return jsonObj['status'];
+  }
+
+  static Future<bool> deleteProduk({int? id}) async {
+    String apiUrl = ApiUrl.deleteProduk(id!);
+    var response = await Api().delete(apiUrl);
+
+    // Memeriksa status kode HTTP
+    if (response.statusCode == 200 || response.statusCode == 204) {
+      // Hapus berhasil, pastikan mengembalikan nilai yang sesuai
+      return true;
+    } else {
+      // Hapus gagal, log atau tangani kesalahan jika perlu
+      print("Gagal menghapus produk: ${response.body}");
+      return false; // Gagal
+    }
+  }
+}
